a66bab3dbdf10fc21067fb8f1931cb4c34c21ea4
VToribiot <vtoribiot@gmail.com>
2022-05-23 13:41:14 -0400

    More Finishing touches + comments

--- Session2/UnitTest.py
+++ Session2/UnitTest.py
@@ -4,83 +4,83 @@ import session2 as s
 
 class MyTests(u.TestCase):
     def test01(self):  # Conversion Test
-        t1 = s.Temperature(249.4426, "C")
-        t2 = s.Temperature(8.1888, "F")
+        t1 = s.Temperature(249.4426, "Cº")
+        t2 = s.Temperature(8.1888, "Fº")
         t3 = t1.conversion(t2)
         self.assertAlmostEqual(-13.2284444, t3.value, 4)
-        self.assertEqual("C", t3.scale)
+        self.assertEqual("Cº", t3.scale)
 
     def test02(self):  # Conversion Test
-        t1 = s.Temperature(241.0029, "C")
-        t2 = s.Temperature(-270.1657, "K")
+        t1 = s.Temperature(241.0029, "Cº")
+        t2 = s.Temperature(-270.1657, "Kº")
         t3 = t1.conversion(t2)
         self.assertAlmostEqual(-543.3157, t3.value, 4)
-        self.assertEqual("C", t3.scale)
+        self.assertEqual("Cº", t3.scale)
 
-    def test03(self):
-        t1 = s.Temperature(-173.7856, "F")
-        t2 = s.Temperature(317.6799, "K")
+    def test03(self):  # Addition Test
+        t1 = s.Temperature(-173.7856, "Fº")
+        t2 = s.Temperature(317.6799, "Kº")
         t3 = t1.add(t2)
         self.assertAlmostEqual(-61.63178, t3.value, 4)
-        self.assertEqual("F", t3.scale)
+        self.assertEqual("Fº", t3.scale)
 
-    def test04(self):
-        t1 = s.Temperature(-43.0363, "K")
-        t2 = s.Temperature(365.3039, "F")
+    def test04(self):  # Addition Test
+        t1 = s.Temperature(-43.0363, "Kº")
+        t2 = s.Temperature(365.3039, "Fº")
         t3 = t1.add(t2)
         self.assertAlmostEqual(415.28253333, t3.value, 4)
-        self.assertEqual("K", t3.scale)
+        self.assertEqual("Kº", t3.scale)
 
-    def test05(self):
-        t1 = s.Temperature(64.1594, "F")
-        t2 = s.Temperature(-422.8018, "F")
+    def test05(self):  # Substraction Test
+        t1 = s.Temperature(64.1594, "Fº")
+        t2 = s.Temperature(-422.8018, "Fº")
         t3 = t1.substract(t2)
         self.assertAlmostEqual(486.9612, t3.value, 4)
-        self.assertEqual("F", t3.scale)
+        self.assertEqual("Fº", t3.scale)
 
-    def test06(self):
-        t1 = s.Temperature(219.5862, "K")
-        t2 = s.Temperature(-409.4712, "C")
+    def test06(self):  # Subtraction Test
+        t1 = s.Temperature(219.5862, "Kº")
+        t2 = s.Temperature(-409.4712, "Cº")
         t3 = t1.substract(t2)
         self.assertAlmostEqual(355.9074, t3.value, 4)
-        self.assertEqual("K", t3.scale)
+        self.assertEqual("Kº", t3.scale)
 
-    def test07(self):
-        t1 = s.Temperature(294.3932, "F")
-        t2 = s.Temperature(19.1793, "C")
+    def test07(self):  # Multiplication Test
+        t1 = s.Temperature(294.3932, "Fº")
+        t2 = s.Temperature(19.1793, "Cº")
         t3 = t1.multiply(t2)
         self.assertAlmostEqual(19583.8423, t3.value, 4)
-        self.assertEqual("F", t3.scale)
+        self.assertEqual("Fº", t3.scale)
 
-    def test08(self):
-        t1 = s.Temperature(75.8430, "C")
-        t2 = s.Temperature(190.5469, "K")
+    def test08(self):  # Multiplication Test
+        t1 = s.Temperature(75.8430, "Cº")
+        t2 = s.Temperature(190.5469, "Kº")
         t3 = t1.multiply(t2)
         self.assertAlmostEqual(-6264.8669, t3.value, 4)
-        self.assertEqual("C", t3.scale)
+        self.assertEqual("Cº", t3.scale)
 
-    def test09(self):
-        t1 = s.Temperature(-11.5765, "C")
-        t2 = s.Temperature(-32.7466, "C")
+    def test09(self):  # Division Test
+        t1 = s.Temperature(-11.5765, "Cº")
+        t2 = s.Temperature(-32.7466, "Cº")
         t3 = t1.divide(t2)
         self.assertAlmostEqual(0.3535, t3.value, 4)
-        self.assertEqual("C", t3.scale)
+        self.assertEqual("Cº", t3.scale)
 
-    def test10(self):
-        t1 = s.Temperature(234.6318, "F")
-        t2 = s.Temperature(222.5054, "K")
+    def test10(self):  # Division Test
+        t1 = s.Temperature(234.6318, "Fº")
+        t2 = s.Temperature(222.5054, "Kº")
         t3 = t1.divide(t2)
         self.assertAlmostEqual(-3.9660, t3.value, 4)
-        self.assertEqual("F", t3.scale)
+        self.assertEqual("Fº", t3.scale)
 
-    def test11(self):
-        t1 = s.Temperature(-257.4078, "C")
-        t2 = s.Temperature(257.4078, "C")
+    def test11(self):  # Zero result Test
+        t1 = s.Temperature(-257.4078, "Cº")
+        t2 = s.Temperature(257.4078, "Cº")
         self.assertRaises(ValueError, t1.add, t2)
 
-    def test12(self):
-        t1 = s.Temperature(71.5363, "F")
-        t2 = s.Temperature(-17.7778, "C")
+    def test12(self):  # Zero Division Test
+        t1 = s.Temperature(71.5363, "Fº")
+        t2 = s.Temperature(-17.7778, "Cº")
         self.assertRaises(BaseException, t1.divide(t2))
 
 
--- Session2/session2.py
+++ Session2/session2.py
@@ -1,52 +1,52 @@
 class Temperature:
     def __init__(self, value, scale):
-        self.value = value
-        self.scale = scale
+        self.value = value  # Numerical Value
+        self.scale = scale  # Scales being C -> Celcius, K -> Kelvin and F -> Fahrenheit
 
     def conversion(self, o):
-        if self.scale == o.scale:
+        if self.scale == o.scale:  # Checks if both scales are the same
             return o
-        elif self.scale == "C":
-            if o.scale == "F":
-                return Temperature((o.value - 32) * 5/9, "C")
-            elif o.scale == "K":
-                return Temperature((o.value - 273.15), "C")
-        elif self.scale == "K":
-            if o.scale == "F":
-                return Temperature(((o.value - 32) * 5/9) + 273.15, "K")
-            elif o.scale == "C":
-                return Temperature(o.value + 273.15, "K")
-        elif self.scale == "F":
-            if o.scale == "C":
-                return Temperature((o.value * 9/5) + 32, "F")
-            elif o.scale == "K":
-                return Temperature(((o.value - 273.15) * 9/5) + 32, "F")
+        elif self.scale == "Cº":  # If main scale is Celcius
+            if o.scale == "Fº":  # If secondary scale is Fahrenheit
+                return Temperature((o.value - 32) * 5/9, "Cº")
+            elif o.scale == "Kº":  # If secondary scale is Kelvin
+                return Temperature((o.value - 273.15), "Cº")
+        elif self.scale == "Kº":  # If main scale is Kelvin
+            if o.scale == "Fº":  # If secondary scale is Fahrenheit
+                return Temperature(((o.value - 32) * 5/9) + 273.15, "Kº")
+            elif o.scale == "Cº":  # If secondary scale is Celcius
+                return Temperature(o.value + 273.15, "Kº")
+        elif self.scale == "Fº":  # If main scale is Fahrenheit
+            if o.scale == "Cº":  # If secondary scale is Celcius
+                return Temperature((o.value * 9/5) + 32, "Fº")
+            elif o.scale == "Kº":  # If secondary scale is Kelvin
+                return Temperature(((o.value - 273.15) * 9/5) + 32, "Fº")
 
     def add(self, o):
-        o = self.conversion(o)
+        o = self.conversion(o)  # Conversion method invocation
         if round(self.value + o.value, 4) != 0:
-            return Temperature(round(self.value + o.value, 4), self.scale)
+            return Temperature(round(self.value + o.value, 4), self.scale)  # Resultant Object
         else:
             raise ValueError
 
     def substract(self, o):
-        o = self.conversion(o)
+        o = self.conversion(o)  # Conversion method invocation
         if round(self.value - o.value, 4) != 0:
-            return Temperature(round(self.value - o.value, 4), self.scale)
+            return Temperature(round(self.value - o.value, 4), self.scale)  # Resultant Object
         else:
             raise ValueError
 
     def multiply(self, o):
-        o = self.conversion(o)
+        o = self.conversion(o)  # Conversion method invocation
         if self.value * o.value != 0:
-            return Temperature(round((self.value * o.value), 4), self.scale)
+            return Temperature(round((self.value * o.value), 4), self.scale)  # Resultant Object
         else:
             raise ValueError
 
     def divide(self, o):
-        o = self.conversion(o)
+        o = self.conversion(o)  # Conversion method invocation
         if round(self.value / o.value, 4) != 0 and o.value != 0:
-            return Temperature(round((self.value / o.value), 4), self.scale)
+            return Temperature(round((self.value / o.value), 4), self.scale)  # Resultant Object
         elif round(o.value, 4) == 0 or round(self.value, 4) == 0:
             raise ZeroDivisionError
         else:
