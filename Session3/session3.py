e086839e484d52bc182396c1eeeaf3c9e16f66f8
VToribiot <vtoribiot@gmail.com>
2022-05-24 12:00:07 -0400

    conversion method and class created

---
+++ Session3/session3.py
@@ -0,0 +1,20 @@
+class Temperature:
+    def __init__(self, value, scale):
+        self.value = value
+        self.scale = scale
+
+    def conversion(self, o):
+        if self.scale == o.scale:
+            return o
+        elif self.scale == "C" and o.scale == "F":
+            return o((o.value - 32) * 5/9, self.scale)
+        elif self.scale == "C" and o.scale == "K":
+            return o(o.value - 273.15, self.scale)
+        elif self.scale == "F" and o.scale == "C":
+            return o((o.value * 9/5) + 32, self.scale)
+        elif self.scale == "F" and o.scale == "K":
+            return o(((o.value - 273.15) * 9/5) + 32, self.scale)
+        elif self.scale == "K" and o.scale == "C":
+            return o(o.value + 273.15, self.scale)
+        elif self.scale == "K" and o.scale == "F":
+            return o(((o.value - 32) * 5/9) + 273.15, self.scale)
