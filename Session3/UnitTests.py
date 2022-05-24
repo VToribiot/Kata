230c41809192b5dfb906a10527683b055d26c9d0
VToribiot <vtoribiot@gmail.com>
2022-05-24 12:06:56 -0400

    Basic unittest file created

---
+++ Session3/UnitTests.py
@@ -0,0 +1,11 @@
+import unittest as u
+import session3 as s
+
+
+class MyTestCase(u.TestCase):
+    def test_something(self):
+        self.assertEqual(True, False)  # add assertion here
+
+
+if __name__ == '__main__':
+    u.main()
