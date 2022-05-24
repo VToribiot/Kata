80cc1f75998e867c8ed1c1f3f6bb81cfca16a23b
VToribiot <vtoribiot@gmail.com>
2022-05-24 12:11:17 -0400

    test cases setup

--- Session3/UnitTests.py
+++ Session3/UnitTests.py
@@ -3,9 +3,91 @@ import session3 as s
 
 
 class MyTestCase(u.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+    def test01(self):
+        t1 = s.Temperature()
+        t2 = s.Temperature()
+        t3 = t1.add(t2)
+        self.assertAlmostEqual( , t3.value)
+        self.assertEqual(t1.scale, t3.scale)
+
+    def test02(self):
+        t1 = s.Temperature()
+        t2 = s.Temperature()
+        t3 = t1.add(t2)
+        self.assertAlmostEqual( , t3.value)
+        self.assertEqual(t1.scale, t3.scale)
+
+    def test03(self):
+        t1 = s.Temperature()
+        t2 = s.Temperature()
+        t3 = t1.add(t2)
+        self.assertAlmostEqual( , t3.value)
+        self.assertEqual(t1.scale, t3.scale)
+
+    def test04(self):
+        t1 = s.Temperature()
+        t2 = s.Temperature()
+        t3 = t1.add(t2)
+        self.assertAlmostEqual( , t3.value)
+        self.assertEqual(t1.scale, t3.scale)
+
+    def test05(self):
+        t1 = s.Temperature()
+        t2 = s.Temperature()
+        t3 = t1.add(t2)
+        self.assertAlmostEqual( , t3.value)
+        self.assertEqual(t1.scale, t3.scale)
+
+    def test06(self):
+        t1 = s.Temperature()
+        t2 = s.Temperature()
+        t3 = t1.add(t2)
+        self.assertAlmostEqual( , t3.value)
+        self.assertEqual(t1.scale, t3.scale)
+
+    def test07(self):
+        t1 = s.Temperature()
+        t2 = s.Temperature()
+        t3 = t1.add(t2)
+        self.assertAlmostEqual( , t3.value)
+        self.assertEqual(t1.scale, t3.scale)
+
+    def test08(self):
+        t1 = s.Temperature()
+        t2 = s.Temperature()
+        t3 = t1.add(t2)
+        self.assertAlmostEqual( , t3.value)
+        self.assertEqual(t1.scale, t3.scale)
+
+    def test09(self):
+        t1 = s.Temperature()
+        t2 = s.Temperature()
+        t3 = t1.add(t2)
+        self.assertAlmostEqual( , t3.value)
+        self.assertEqual(t1.scale, t3.scale)
+
+    def test10(self):
+        t1 = s.Temperature()
+        t2 = s.Temperature()
+        t3 = t1.add(t2)
+        self.assertAlmostEqual( , t3.value)
+        self.assertEqual(t1.scale, t3.scale)
+
+    def test11(self):
+        t1 = s.Temperature()
+        t2 = s.Temperature()
+        t3 = t1.add(t2)
+        self.assertAlmostEqual( , t3.value)
+        self.assertEqual(t1.scale, t3.scale)
+
+    def test12(self):
+        t1 = s.Temperature()
+        t2 = s.Temperature()
+        t3 = t1.add(t2)
+        self.assertAlmostEqual( , t3.value)
+        self.assertEqual(t1.scale, t3.scale)
 
 
-if __name__ == '__main__':
-    u.main()
+def run():
+    suite = u.TestLoader().loadTestsFromTestCase(MyTestCase)
+    u.TextTestRunner(verbosity=3).run(suite)
